5e05bf269c9706feb7c90578fbed7a5896681156
Martin Petluš <martinpetlus@gmail.com>
2017-10-09 22:20:28 +0200

    Store name to local storage

--- client/src/reducers/name.js
+++ client/src/reducers/name.js
@@ -1,16 +1,21 @@
 import { CHANGE_NAME, JOIN_SESSION_SUCCESS } from '../constants/ActionTypes'
 
+const NAME_KEY = 'name_key'
+
 const DEFAULT_PLAYER_1 = 'Player 1'
 const DEFAULT_PLAYER_2 = 'Player 2'
 
-const initialState = DEFAULT_PLAYER_1
+const initialState = localStorage.getItem(NAME_KEY) || DEFAULT_PLAYER_1
 
 export default function nameReducer(state = initialState, action) {
   switch (action.type) {
-    case CHANGE_NAME:
-      return action.payload
+    case CHANGE_NAME: {
+      const newName = action.payload
+      localStorage.setItem(NAME_KEY, newName)
+      return newName
+    }
     case JOIN_SESSION_SUCCESS:
-      return DEFAULT_PLAYER_2
+      return localStorage.getItem(NAME_KEY) || DEFAULT_PLAYER_2
     default:
       return state
   }
